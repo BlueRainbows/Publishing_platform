2ea5e056197c236dea8b5d60e1faa2dca5652c35
hirrumi <BlueberryRainbows@yandex.ru>
2024-06-18 12:13:27 +0300

    Добавление валидации на номер при регистрации и изменении, вывод в поле к комментариям имени пользователя и аватар

--- users/forms.py
+++ users/forms.py
@@ -28,8 +28,8 @@ class UserRegisterForm(StyleFormsMixin, UserCreationForm):
     phone = forms.CharField(
         max_length=12,
         label='Номер телефона',
-        help_text='•Ваш номер должен быть в формате: '
-                  '+71234567890'
+        help_text='<em>* Ваш номер должен быть в формате: '
+                  '<strong>+71234567890</strong></em>'
     )
     email = forms.EmailField(
         label='Электоронная почта'
@@ -37,15 +37,20 @@ class UserRegisterForm(StyleFormsMixin, UserCreationForm):
     password1 = forms.CharField(
         min_length=8,
         label="Придумайте пароль", widget=forms.PasswordInput,
-        help_text='• Ваш пароль не должен быть простым,'
+        help_text='<em>* Ваш пароль не должен быть простым,'
                   ' содержать только цифры и '
-                  'иметь длинну не менее 8-ми символов'
+                  'иметь длинну не менее 8-ми символов</em>'
     )
     password2 = forms.CharField(
         min_length=8,
         label="Повторите пароль", widget=forms.PasswordInput,
-        help_text='• Повторите введенный пароль'
     )
+
+    def clean_phone(self):
+        cleaned_data = self.cleaned_data.get('phone')
+        if not cleaned_data.startswith('+7'):
+            raise forms.ValidationError("Номер телефона должен начинаться c +7")
+        return cleaned_data
 
     class Meta:
         model = User
